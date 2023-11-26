e134accb481537b1bba99a543b4a6eaaf3f8fe3a
gbean-24 <114492298+gbean-24@users.noreply.github.com>
2023-11-26 04:33:43 +0100

    Add files via upload

---
+++ AccessToken.py
@@ -0,0 +1,17 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+}
+
+data = '{"key": "9f53630e104b054678d692c48f1f4fa66e56dca24b", "secret":"1a2fbebc264a4e001616e1"}'
+
+response = requests.post('https://api.imweb.me/v2/auth', headers=headers, data=data)
+
+print(response.json())
+#Access tokens need to be updated everytime when accessing client database
+
+# Sources
+# curl to python converter: https://curlconverter.com/
+# Imweb official document: https://developers.imweb.me/getstarted/token
+# Original code: curl -d '{"key": "{API_KEY}", "secret":"{SECRET}"}' https://api.imweb.me/v2/auth
